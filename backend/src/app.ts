c44725680f9afe2f10e183fe327c7bbe7b5fc301
sakibsarkar <sakibsarkar707@gmail.com>
2024-08-17 12:02:05 +0600

    not found middlewere

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import cors from "cors";
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from "express";
 import http from "http";
 // import morgan from "morgan";
 import connectDB from "./config/db";
 
 import errorMiddleware from "./middlewares/error";
+import { notFound } from "./middlewares/not-found";
 import routes from "./routes/index";
 
 const app: Application = express();
@@ -30,19 +31,7 @@ app.use("/api/v1/", routes);
 app.use(errorMiddleware);
 
 //handle not found
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({
-    success: false,
-    message: "Not Found",
-    errorMessages: [
-      {
-        path: req.originalUrl,
-        message: "API Not Found",
-      },
-    ],
-  });
-  next();
-});
+app.use(notFound);
 
 const port: any = process.env.PORT || 5000;
 
